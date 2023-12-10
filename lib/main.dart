8e1317e23ff8409e19c097eab0efa8bab1c68369
kiichi7580 <kiichi.nakazato0521@gmail.com>
2023-12-10 23:57:32 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:basics/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: 
+          GradientContainer(
+            Colors.purple,
+            Colors.indigo,
+          ),
+        ),
+      ),
+    );
+}
